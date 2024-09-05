de0a393a6dfc901d32dc581a03a79cf56b95ae8d
Howard Huang <howardhuang@fb.com>
2024-09-05 13:35:29 -0700

    [WIP] zero bubble
    ghstack-source-id: d813ea1be1375567b1eda4f418bfae9d6fbdd84c
    Pull Request resolved: https://github.com/pytorch/torchtitan/pull/546

--- torchtitan/parallelisms/pipeline_llama.py
+++ torchtitan/parallelisms/pipeline_llama.py
@@ -12,7 +12,7 @@ from typing import Callable, Union
 import torch
 import torch.nn as nn
 from torch.distributed import DeviceMesh
-from torch.distributed.pipelining import PipelineStage
+from .pipelining import PipelineStage
 
 from torchtitan.config_manager import JobConfig, TORCH_DTYPE_MAP
 from torchtitan.logging import logger
@@ -42,6 +42,7 @@ def pipeline_llama(
 
     pp_schedule = build_pipeline_schedule(job_config, stages, loss_fn)
 
+    logger.info("Applied PP to the model")
     return pp_schedule, models
 
 
@@ -137,6 +138,7 @@ def pipeline_llama_manual_split(
             input_args=input.chunk(microbatches)[0],
             output_args=output.chunk(microbatches)[0],
             group=pp_mesh.get_group("pp"),
+            # dw_builder=lambda: lambda: print("dummy dw_runner"),
         )
         return stage, model
 
@@ -145,7 +147,12 @@ def pipeline_llama_manual_split(
 
     stages = []
     models = []
-    for stage_idx in stage_ids_this_rank(pp_rank, pp_size, num_stages, style="loop"):
+    loop_style = (
+        "v" if job_config.experimental.pipeline_parallel_schedule == "zb_v" else "loop"
+    )
+    for stage_idx in stage_ids_this_rank(
+        pp_rank, pp_size, num_stages, style=loop_style
+    ):
         start_layer = splits[stage_idx - 1] if stage_idx > 0 else None
         stop_layer = splits[stage_idx] if stage_idx < num_stages - 1 else None
         stage, model_chunk = _build_stage(
